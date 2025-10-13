6a98c7fde10bf30627cc23b87ef00c705f601be0
saurabht2 <saurabh.t@idcglobal.com>
2025-10-13 12:27:48 +0530

    FIX: CORS

--- Service/index.js
+++ Service/index.js
@@ -10,23 +10,13 @@ import ensureDBConnection from './middleware/ensureDBConnection.js'; // Add this
 
 const app = express();
 
-const allowedOrigins = [
-  'https://todo-s-be.vercel.app',
-  'http://localhost:5173',
-];
-
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
+app.use(
+  cors({
+    origin: "https://todo-s-be.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 app.use(ensureDBConnection); // Add this middleware before routes
